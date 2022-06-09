75326195fed2103c49e862c8b5327c83edc7655c
GustavoBolanoss <gustavo.bolanho@sptech.school>
2022-06-09 10:29:39 -0300

    Ultima versão

--- Site individualVersao5APIZADA/src/database/script-tabelas.sql
+++ Site individualVersao5APIZADA/src/database/script-tabelas.sql
@@ -2,6 +2,34 @@
 -- Você precisa executar os comandos no banco de dados para criar as tabelas,
 -- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
 /* para workbench - local - desenvolvimento */
+
+
+CREATE DATABASE blackmagic;
+
+USE blackmagic;
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50)
+);
+
+create table deck(
+idDeck int auto_increment,
+fkUsuario int,
+foreign key (fkUsuario) references usuario (id),
+primary key(idDeck, fkUsuario),
+cor varchar(45),
+numDeck int,
+qtdLands int,
+qtdArtefatos int,
+qtdEncatamentos int,
+qtdCriaturas int,
+qtdInstant int,
+qtdFeitiços int
+);
+
 CREATE DATABASE acquatec;
 
 USE acquatec;
